d0cd8183c1ac34af542f15f42e2cebbe72272d87
PRITAM CHAKROBORTY <chakrobortypritam1@gmail.com>
2025-04-30 23:40:56 +0530

    retriever added

--- webLoader.py
+++ webLoader.py
@@ -2,7 +2,7 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from langchain_chroma import Chroma
+from langchain.vectorstores import Chroma
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -32,7 +32,7 @@ documents = [
     {
         "pageContent": text,
         "metadata": {
-            "id": str(index),           # Convert index to string
+            "id": str(index),          
             "chunkIndex": index,
         }
     }
