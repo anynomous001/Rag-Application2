75c55a2866302eb2ad923a26a8e2c5075c34ed22
PRITAM CHAKROBORTY <chakrobortypritam1@gmail.com>
2025-05-02 23:02:47 +0530

    qdrant installed

--- chat.py
+++ chat.py
@@ -2,7 +2,6 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from langchain.vectorstores import Chroma
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import ChatPromptTemplate
